ea2b1b13cc941ca8782b0a900565b12a82900b1d
silverSapphire <u0844864@utah.edu>
2021-12-11 04:57:03 +0000

    Day 4.1 complete

--- day4.1/target/debug/day1.d
+++ day4.1/target/debug/day1.d
@@ -1,1 +1,1 @@
-/home/ky/aoc21/day3.2/target/debug/day1: /home/ky/aoc21/day3.2/src/main.rs
+/home/ky/aoc21/day4.1/target/debug/day1: /home/ky/aoc21/day4.1/src/main.rs
--- day4.1/target/debug/deps/day1-f6bf04934a365449.d
+++ day4.1/target/debug/deps/day1-f6bf04934a365449.d
@@ -1,5 +1,5 @@
-/home/ky/aoc21/day3.2/target/debug/deps/day1-f6bf04934a365449: src/main.rs
+/home/ky/aoc21/day4.1/target/debug/deps/day1-f6bf04934a365449: src/main.rs
 
-/home/ky/aoc21/day3.2/target/debug/deps/day1-f6bf04934a365449.d: src/main.rs
+/home/ky/aoc21/day4.1/target/debug/deps/day1-f6bf04934a365449.d: src/main.rs
 
 src/main.rs:
